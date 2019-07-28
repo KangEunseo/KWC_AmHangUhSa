d702c711a70562baffdedea3d1d18c87e753c7c4
KangEunseo <s2018w21@e-mirim.hs.kr>
2019-07-29 00:53:38 +0900

    mobile web

--- js/index.js
+++ js/index.js
@@ -23,3 +23,31 @@ function clickbutton(){
       nodeMenuButton: nodeMenu.querySelector('.js-menu__toggle')
     });
 })();
+
+function char1(){
+  var v = document.getElementsByClassName('char');
+
+  for(var i=0;i<v.length;i++){
+    v[i].src = "image/char1_face.jpg";
+  }
+}
+
+function char2(){
+  var v = document.getElementsByClassName('char');
+
+  for(var i=0;i<v.length;i++){
+    v[i].src = "image/char2_face.jpg";
+  }
+
+  alert("출시되지 않은 캐릭터입니다.");
+}
+
+function char3(){
+  var v = document.getElementsByClassName('char');
+
+  for(var i=0;i<v.length;i++){
+    v[i].src = "image/char3_face.jpg";
+  }
+
+  alert("출시되지 않은 캐릭터입니다.");
+}
